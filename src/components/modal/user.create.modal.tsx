eed200672105b53631f1f8b95c47c39943078d84
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-08-27 18:33:45 +0700

    create

--- src/components/modal/user.create.modal.tsx
+++ src/components/modal/user.create.modal.tsx
@@ -1,70 +1,100 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const UserCreateModal = (props: any) => {
-    const { isOpenCreateModal, setIsOpenCreateModal } = props;
+  const { isOpenCreateModal, setIsOpenCreateModal } = props;
 
-    const [email, setEmail] = useState<string>("");
-    const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
 
-    const handleSubmit = () => {
-        if (!email) {
-            alert("email empty");
-            return;
-        }
-        if (!name) {
-            alert("name empty");
-            return;
-        }
-        //call api => call redux
-        console.log({ email, name }) //payload
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (payload: any) => {
+      return fetch("http://localhost:8000/users", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+
+        //make sure to serialize your JSON body
+        body: JSON.stringify({
+          name: payload.name,
+          email: payload.email,
+        }),
+      }).then((response) => {
+        //do something awesome that makes the world a better place
+      });
+    },
+
+    // hỏi vì sao mà cái này sai nhưng vẫn set có dữ liệu cho đến khi quá trang
+    // hỏi thêm vì sao nó là data cũ
+    onSuccess: (data, variables, context) => {
+      setIsOpenCreateModal(false);
+      queryClient.invalidateQueries({ queryKey: ["repoData"] });
+    },
+  });
+  const handleSubmit = () => {
+    if (!email) {
+      alert("email empty");
+      return;
     }
+    if (!name) {
+      alert("name empty");
+      return;
+    }
+    //call api => call redux
+    console.log({ email, name }); //payload
 
-    return (
-        <>
-            <Modal
-                show={isOpenCreateModal}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                backdrop={false}
-                onHide={() => setIsOpenCreateModal(false)}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>
-                        Add A New User
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <FloatingLabel
-                        label="Email"
-                        className="mb-3"
-                    >
-                        <Form.Control
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="text"
-                        />
-                    </FloatingLabel>
-                    <FloatingLabel label="Name">
-                        <Form.Control
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            type="text"
-                        />
-                    </FloatingLabel>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button
-                        variant='warning'
-                        onClick={() => setIsOpenCreateModal(false)} className='mr-2'>Cancel</Button>
-                    <Button onClick={() => handleSubmit()}>Save</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    )
-}
+    mutation.mutate({ email, name });
+  };
+
+  return (
+    <>
+      <Modal
+        show={isOpenCreateModal}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        backdrop={false}
+        onHide={() => setIsOpenCreateModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add A New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel label="Email" className="mb-3">
+            <Form.Control
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+            />
+          </FloatingLabel>
+          <FloatingLabel label="Name">
+            <Form.Control
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+            />
+          </FloatingLabel>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="warning"
+            onClick={() => setIsOpenCreateModal(false)}
+            className="mr-2"
+          >
+            Cancel
+          </Button>
+          <Button onClick={() => handleSubmit()}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default UserCreateModal;
